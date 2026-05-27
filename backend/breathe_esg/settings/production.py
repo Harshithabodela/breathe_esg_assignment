1c78a3426fbaea7b2fd33c031c37b386aa40f459
Harshithabodela <harshithareddybodela@gmail.com>
2026-05-27 23:53:42 +0530

    fix database config for railway

--- backend/breathe_esg/settings/production.py
+++ backend/breathe_esg/settings/production.py
@@ -15,3 +15,10 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+import dj_database_url
+DATABASE_URL = config("DATABASE_URL", default="")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
