41bb95be5f5f4c2671deb08ff909340a62fab39e
Harshithabodela <harshithareddybodela@gmail.com>
2026-05-28 15:22:02 +0530

    fix session cookies for cross-domain

--- backend/breathe_esg/settings/production.py
+++ backend/breathe_esg/settings/production.py
@@ -19,3 +19,6 @@ if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
+
+SESSION_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SAMESITE = "None"
