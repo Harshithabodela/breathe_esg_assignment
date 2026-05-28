f4ba6e72b86a14c4e84644c094dcccad5aa6662f
Harshithabodela <harshithareddybodela@gmail.com>
2026-05-28 14:51:06 +0530

    fix db config using os.environ

--- backend/breathe_esg/settings/production.py
+++ backend/breathe_esg/settings/production.py
@@ -1,13 +1,21 @@
 from .base import *
 from decouple import config
+import dj_database_url
+import os
 
 DEBUG = False
 SECRET_KEY = config("SECRET_KEY")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
-CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="").split(",")
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="https://breathe-esg-production-1d54.up.railway.app").split(",")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
