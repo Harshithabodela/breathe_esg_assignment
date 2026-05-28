7ae34b6ac79184daff7f014b6e45eb0bb5b2e637
Harshithabodela <harshithareddybodela@gmail.com>
2026-05-28 14:47:05 +0530

    fix production settings

--- backend/breathe_esg/settings/production.py
+++ backend/breathe_esg/settings/production.py
@@ -3,22 +3,11 @@ from decouple import config
 
 DEBUG = False
 SECRET_KEY = config("SECRET_KEY")
-
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="").split(",")
 CORS_ALLOW_CREDENTIALS = True
-
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
-
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-import dj_database_url
-DATABASE_URL = config("DATABASE_URL", default="")
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
